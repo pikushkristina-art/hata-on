81f773e13cc66737bdde6fd0d9e08f3cfc4c5393
pikushkristina-art <pikushkristina@gmail.com>
2026-08-24 15:47:51 +0300

    Add store-config.js

---
+++ store-config.js
@@ -0,0 +1,8 @@
+// Додайте контакт продавця перед прийманням реальних замовлень.
+// Приклад Telegram: telegram: "hata_on_shop" (без символу @).
+window.HATA_ON_CONFIG = {
+  telegram: "",
+  phone: "",
+  email: ""
+};
+
